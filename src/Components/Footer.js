6ef76735d3e95b8e85763d3c8d7b0db60f20f9b8
Gaurav1924 <90563290+Gaurav1924@users.noreply.github.com>
2024-07-11 11:27:38 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="mt-4 bg-dark text-white">
       <div className="container p-4 text-center">
-        <h2>Design And Developed By : TECHINFOYT </h2>
+        <h2>Design And Developed By : Gaurav </h2>
         <h6>All Right Reserved by &copy; Techinfoty </h6>
       </div>
     </div>
